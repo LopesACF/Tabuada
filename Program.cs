6071272a37c1a3de5d581857e130dd1d9837ace3
Altemilson Lopes <altemilsonlopes@gmail.com>
2023-06-10 23:08:20 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.Blue;
+
+Console.WriteLine("--- Tabuada ---");
+
+Console.WriteLine("Digite o numero.");
+
+int resultado;
+int fator2 = 0;
+
+int fator1 = Convert.ToInt32(Console.ReadLine()!.Trim());
+
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+while (fator2 <= 10){
+    resultado = fator1 * fator2;
+    Console.WriteLine($"{fator1}x{fator2} = {resultado}");
+    fator2 += 1;
+}
+
+Console.ResetColor();
